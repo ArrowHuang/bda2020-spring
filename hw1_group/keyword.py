4273a115c73ad4b83b92bc1bac92a9d3515e5623
ArrowHuang <wahjttw@gmail.com>
2020-03-31 16:54:17 +0800

    Update keyword extraction code

--- hw1_group/keyword.py
+++ hw1_group/keyword.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 from LDA.lda import LDA_preprocess
 from TextRank.textrank import TextRankAlgo
+from Word2Vec.w2v import word2vec
 
 def get_keyword():
-    LDA_preprocess('sum_end.xlsx','LDA_output.xlsx','stopWord.txt')
-
+    # LDA_preprocess('sum_end.xlsx','LDA_output.xlsx','stopWord.txt')
+    word2vec('sum_end.xlsx','Word2Vec_output.xlsx','stopWord.txt')
 
 
 if __name__ == "__main__":
